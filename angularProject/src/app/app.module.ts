32c701948e1c0378c7f43b92dc551b93ae4d7241
eslam el-shenawy <i.a.shenawy@gmail.com>
2020-05-10 22:50:28 +0200

    NavBar commit

---
+++ angularProject/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StudentsComponent } from './students/students.component';
+import { AboutComponent } from './about/about.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    StudentsComponent,
+    AboutComponent,
+    AddStudentComponent,
+    StudentProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '' , component : StudentsComponent },
+      { path: 'About' , component : AboutComponent },
+      { path: 'AddStudent' , component : AddStudentComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
